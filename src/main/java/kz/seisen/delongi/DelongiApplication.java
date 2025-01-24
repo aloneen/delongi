bdfe7f4c8ae43e388b5bee355b854ef2e5271ed4
aloneen <seisenbek2005@mail.ru>
2025-01-24 16:51:21 +0500

    initial commit

---
+++ src/main/java/kz/seisen/delongi/DelongiApplication.java
@@ -0,0 +1,13 @@
+package kz.seisen.delongi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelongiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelongiApplication.class, args);
+    }
+
+}
